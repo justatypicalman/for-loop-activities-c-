4964dd52d5f663801fa94dd96ffaa1dacbeb7991
Eric Dango <73502965+justatypicalman@users.noreply.github.com>
2020-12-08 06:32:34 +0800

    Update forloopno4.cpp

--- forloopno4.cpp
+++ forloopno4.cpp
@@ -6,8 +6,35 @@ int main(){
 	int i, number,ending,start,count, sum=0;
 	cout << "Enter a starting number of range: ";
 	cin >> start;
+	while(1){
+	if(cin.fail()){
+	cin.clear();
+	cin.ignore(numeric_limits < streamsize > ::max(), '\n');
+	cout<< "Letters are not allowed, numbers only"<<endl;
+	cout<<"Enter a starting number of range: ";
+	cin>>start;
+	}
+	if(!cin.fail())
+	break;
+	}
 	cout << "Enter ending number of range: ";
 	cin >> ending;
+	while(1){
+	if(cin.fail()){
+	cin.clear();
+	cin.ignore(numeric_limits < streamsize > ::max(), '\n');
+	cout<< "Letters are not allowed, numbers only"<<endl;
+	cout<<"Enter ending number of range: ";
+	cin>>ending;
+	}
+	if(!cin.fail())
+	break;
+	}
+	if (start>=ending){
+	cout<< "You have to enter a bigger number than the starting number"<<endl;
+	cout<<"Try again!";
+	return 0;
+	}
 	cout<<"The Sum of Prime numbers from "<<start<<" to "<<ending<< " =";
 	for(number=start;number<=ending;number++)
 	{
